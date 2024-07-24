b32fa0bedeaea2e005e5020c9440bbf04b562349
Rümeysa Tokgöz <126255709+tokgozr@users.noreply.github.com>
2024-07-24 23:23:24 +0300

    Time: 60 ms (13.76%), Space: 34.6 MB (27.22%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+
+       var y = x.ToString().ToCharArray();
+       Array.Reverse(y);
+       return x.ToString() == new string(y);
+        
+        
+    }
+}
